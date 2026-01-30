7eef9bd96f644323b131ad2ead2fe11e1203a0fc
DevanshKumawat <devanshkumawat07@gmail.com>
2026-01-30 02:15:00 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 19847
+let accountEmail = "devansh@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+let accountName ; 
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
