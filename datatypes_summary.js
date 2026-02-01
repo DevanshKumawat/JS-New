82933185132248d1d33f33e4d4e01865bb27ce08
DevanshKumawat <devanshkumawat07@gmail.com>
2026-02-01 01:30:19 +0000

    datatype summary

---
+++ datatypes_summary.js
@@ -0,0 +1,20 @@
+// Primitive Datatypes
+
+// 7 Types : String, Number, Boolean, null, undefined, Symbol, BigInt
+
+
+//Reference (Non Primitive)
+
+// Array, Objects, Functions
+
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "devansh",
+    age : 18,
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
