829141369ef7f20ee2a5596b784ffc3aa14a56bf
DevanshKumawat <devanshkumawat07@gmail.com>
2026-02-01 00:24:20 +0000

    conversion

---
+++ 01_Basics/03_conversionOperations.js
@@ -0,0 +1,31 @@
+// let score = "32abc"
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)  //Convert any type of data into Number (String can also be used)
+// console.log(typeof valueInNumber); 
+// console.log(valueInNumber);        //It returns NaN(not a number) because the entered thing is not a number 
+
+
+// Converts "33" => 33
+// "33abc" => NaN
+// true = 1 and false = 0
+
+
+// let isLoggedIn = 3
+
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true ; 0 => false
+// "" => false
+// "devansh" => true
+
+
+let someNumber = 35
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
