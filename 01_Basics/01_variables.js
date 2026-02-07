00db2a3c3794f49d290e9b3d8762a074488fa16d
DevanshKumawat <devanshkumawat07@gmail.com>
2026-02-07 10:05:00 +0000

    Practice

--- 01_Basics/01_variables.js
+++ 01_Basics/01_variables.js
@@ -1,10 +1,25 @@
-const accountId = 19847
+// const accountId = 19847
+// let accountEmail = "devansh@gmail.com"
+// var accountPassword = "12345"
+// accountCity = "Jaipur"
+
+// let accountName ; 
+
+// console.log(accountId);
+
+// console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
+
+
+
+
+const accountId = 12345
 let accountEmail = "devansh@gmail.com"
 var accountPassword = "12345"
-accountCity = "Jaipur"
+accountCity = "Delhi"
 
-let accountName ; 
+let accountName ;
 
 console.log(accountId);
 
-console.table([accountId, accountEmail, accountPassword, accountCity, accountName])
+
+console.table([ typeof accountId, typeof accountEmail, typeof accountPassword, typeof accountCity, typeof accountName])
